ac476669a9e6c7a23fa2cfbbec2eb45db2133f77
shivam70301 <shivam70301@gmail.com>
2025-07-09 11:10:18 +0530

    store base trmplates

--- store/views.py
+++ store/views.py
@@ -6,3 +6,6 @@ from .models import Product
 def home(request):
     products = Product.objects.all() 
     return render (request, 'home.html',{'products':products})
+
+def about(request):
+    return render(request, 'about.html', {})
